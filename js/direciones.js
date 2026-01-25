42658873af9d0ecbd6e9454a76efa2c6e2717a7e
Bryant Facenda <bryantffacen@gmail.com>
2026-01-24 23:05:29 -0400

    actuualizacion de apariencia del portafolio
    🌙

--- js/direciones.js
+++ js/direciones.js
@@ -1,22 +1,21 @@
-
-/** 
+/**
  * evento del boton curriculum
-*/
-const btnCurriculum = document.getElementById("btnCurriculum")
-if(btnCurriculum){
-    btnCurriculum.addEventListener("click", (e)=>{
-    e.preventDefault()
-    window.location.href = `./src/page/curiculum.html`
-})
+ */
+const btnCurriculum = document.getElementById("btnCurriculum");
+if (btnCurriculum) {
+  btnCurriculum.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = `./src/page/curiculum.html`;
+  });
 }
 
-/** 
+/**
  * evento del boton volver
-*/
-const btnBack = document.getElementById("btnBack")
-if(btnBack){
-    btnBack.addEventListener("click", (e)=>{
-    e.preventDefault()
-    window.location.href = `../../index.html`
-})
+ */
+const btnBack = document.getElementById("btnBack");
+if (btnBack) {
+  btnBack.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = `../../index.html`;
+  });
 }
